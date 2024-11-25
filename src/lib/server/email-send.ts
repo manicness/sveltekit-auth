291c8ffb39a8b176c0b0b161b04cdb3e58601179
manicness <michael@manicness.com>
2024-11-25 18:14:47 +0000

    AWS -> Apple SMTP

--- src/lib/server/email-send.ts
+++ src/lib/server/email-send.ts
@@ -1,11 +1,9 @@
 import nodemailer from 'nodemailer';
 import * as aws from '@aws-sdk/client-ses';
 import {
-	FROM_EMAIL,
-	AWS_ACCESS_KEY_ID,
-	AWS_SECRET_ACCESS_KEY,
-	AWS_REGION,
-	AWS_API_VERSION
+	APPLE_USERNAME,
+	APPLE_PASSWORD,
+	FROM_EMAIL
 } from '$env/static/private';
 //import { z } from "zod";
 export default async function sendEmail(
@@ -14,24 +12,17 @@ export default async function sendEmail(
 	bodyHtml?: string,
 	bodyText?: string
 ) {
-	const hasAccessKeys = AWS_ACCESS_KEY_ID && AWS_SECRET_ACCESS_KEY;
-
-	const ses = new aws.SES({
-		apiVersion: AWS_API_VERSION,
-		region: AWS_REGION,
-		...(hasAccessKeys
-			? {
-					credentials: {
-						accessKeyId: AWS_ACCESS_KEY_ID || '',
-						secretAccessKey: AWS_SECRET_ACCESS_KEY || ''
-					}
-				}
-			: {})
-	});
+	const hasAccessKeys = APPLE_USERNAME && APPLE_PASSWORD;
 
 	// create Nodemailer SES transporter
 	const transporter = nodemailer.createTransport({
-		SES: { ses, aws }
+		host: "smtp.mail.me.com",
+		port: 465,
+		secure: true,
+		auth: {
+			user: APPLE_USERNAME,
+			pass: APPLE_PASSWORD,
+		}
 	});
 
 	try {
